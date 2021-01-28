e7629ca085bc8fc83e75fc3033a029b14213f9bd
axcelcorna <63078897+axcelcorona@users.noreply.github.com>
2021-01-28 16:31:49 -0500

    Creacion del controlador add del modelo user

---
+++ controllers/userController.js
@@ -0,0 +1,16 @@
+import model from '../models';
+
+
+export default{
+    add: async (req, res, next) => {
+        try {
+            const reg = await model.Usuario.create(req.body);
+            res.status(200).json(reg);
+        } catch (error) {
+            res.status(500).send({
+                message: 'Ocurrio un error'
+            });
+            next(error);
+        }
+    }
+};
